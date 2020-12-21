fe7ddf314e520a1254b26bee1015f2ab823a49ec
teemoking <AK1028430241@gmail.com>
2020-12-21 20:07:34 +0800

    rebuilding site 2020年12月21日 20:07:34

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# build the project
+#hugo
+git add .
+
+msg="rebuilding site `date`"
+
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+git commit -m "$msg"
+
+# push source to github
+git push -u superman master
